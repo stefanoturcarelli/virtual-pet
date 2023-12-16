651ca6cf669a545a7446f1e391509ecf144209c4
Stefano <stefano.turcarelli@icloud.com>
2023-12-16 17:47:26 -0600

    Add dark mode toggle functionality

--- assets/script/index.js
+++ assets/script/index.js
@@ -4,9 +4,17 @@ const click = new Audio("assets/media/sounds/click.mp3");
 click.volume = 0.5;
 
 const buttons = document.querySelectorAll(".button");
+const lightBtn = document.querySelector(".light-btn");
+const petContainer = document.querySelector(".pet-container");
+const virtualPet = document.querySelector(".virtual-pet");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     click.play();
   });
+});
+
+lightBtn.addEventListener("click", () => {
+  petContainer.classList.toggle("dark");
+  virtualPet.classList.toggle("hidden");
 });
